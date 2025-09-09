6890a2cc074305aa8f25b471bf625ebec83152dc
lucasferraz122 <lucasfersan10@gmail.com>
2025-09-09 19:14:37 -0300

    Create no.java

---
+++ no.java
@@ -0,0 +1,10 @@
+// classe para representar um nó da lista encadeada
+class No {
+    int valor;
+    No proximo;
+
+    No(int valor) {
+        this.valor = valor;
+        this.proximo = null;
+    }
+}
